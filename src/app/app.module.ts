940775516f64f540e4f3428afb41a53357345a75
Nghia Nguyen <zinzinday@gmail.com>
2017-10-10 12:03:15 +0700

    chore: initial commit from @angular/cli
    Signed-off-by: Nghia Nguyen <zinzinday@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppComponent} from './app.component';
-import {MainComponentComponent} from './main-component/main-component.component';
-import {HeaderComponentComponent} from './header-component/header-component.component';
+
+import {
+  MatCommonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule
+} from '@angular/material';
+
+import {HeaderComponent} from './header/header.component';
+import {SearchComponent} from './header/search/search.component';
+import {NavbarComponent} from './header/navbar/navbar.component';
+import {WrapperComponent} from './wrapper/wrapper.component';
+import {SidebarComponent} from './wrapper/sidebar/sidebar.component';
+import {ContentComponent} from './wrapper/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponentComponent,
-    HeaderComponentComponent,
+    HeaderComponent,
+    SearchComponent,
+    NavbarComponent,
+    WrapperComponent,
+    SidebarComponent,
+    ContentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
