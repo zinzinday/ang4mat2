3e06ba11d30615c444389eadc36495ed34d0ae8d
Nghia Nguyen <zinzinday@gmail.com>
2017-10-10 08:05:06 +0700

    ng g c cName
    Signed-off-by: Nghia Nguyen <zinzinday@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MainComponentComponent} from './main-component/main-component.component';
+import {HeaderComponentComponent} from './header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponentComponent,
+    HeaderComponentComponent,
   ],
   imports: [
     BrowserModule
@@ -13,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
